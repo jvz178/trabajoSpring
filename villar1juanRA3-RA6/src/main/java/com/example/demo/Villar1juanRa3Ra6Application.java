0d3ee8401d0d053dc3bdc36c2790bc6b8b645703
jvz178 <villar.zajua20@cadiz.salesianos.edu>
2021-04-11 11:34:06 +0200

    Primer commit

---
+++ villar1juanRA3-RA6/src/main/java/com/example/demo/Villar1juanRa3Ra6Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Villar1juanRa3Ra6Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Villar1juanRa3Ra6Application.class, args);
+	}
+
+}
