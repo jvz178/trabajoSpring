eca4411c2f6ebfa64197da01bfb50cad89f6d3a4
jvz178 <villar.zajua20@cadiz.salesianos.edu>
2021-04-17 12:13:18 +0200

    c

--- villar1juanRA3-RA6/src/main/java/com/example/demo/repository/RepositorioCliente.java
+++ villar1juanRA3-RA6/src/main/java/com/example/demo/repository/RepositorioCliente.java
@@ -4,11 +4,13 @@ import java.io.Serializable;
 
 
 
+
+
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Repository;
 
 import com.example.demo.entity.Clientes;
+
 
 
 @Repository("repositorioCliente")
