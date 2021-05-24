43591eb07754ffb7312287791bb2081d793af1e8
jvz178 <villar.zajua20@cadiz.salesianos.edu>
2021-05-24 19:00:56 +0200

    commit

--- villar1juanRA3-RA6/src/main/java/com/example/demo/util/CrearPdf.java
+++ villar1juanRA3-RA6/src/main/java/com/example/demo/util/CrearPdf.java
@@ -10,7 +10,9 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.view.document.AbstractPdfView;
 
 import com.example.demo.entity.Citas;
+import com.example.demo.entity.Usuarios;
 import com.lowagie.text.Document;
+import com.lowagie.text.Paragraph;
 import com.lowagie.text.pdf.PdfPTable;
 import com.lowagie.text.pdf.PdfWriter;
 
@@ -23,14 +25,60 @@ public class CrearPdf extends AbstractPdfView{
 		
 		List<Citas> listadoCitas = (List<Citas>) model.get("citas");
 		
-		PdfPTable tablaCitas = new PdfPTable(4);
+		PdfPTable tablaCliente = new PdfPTable(4);
+		tablaCliente.addCell("Nombre");
+		tablaCliente.addCell("Apellidos");
+		tablaCliente.addCell("Telefono");
+		tablaCliente.addCell("Username");
+		tablaCliente.setHeaderRows(1);
 		
+		PdfPTable tablaVeterinarios = new PdfPTable(4);
+		tablaVeterinarios.addCell("Nombre");
+		tablaVeterinarios.addCell("Apellidos");
+		tablaVeterinarios.addCell("Telefono");
+		tablaVeterinarios.addCell("Username");
+		tablaVeterinarios.setHeaderRows(1);
+		
+		PdfPTable tablaMascota = new PdfPTable(5);
+		tablaMascota.addCell("Nombre");
+		tablaMascota.addCell("Tipo");
+		tablaMascota.addCell("Raza");
+		tablaMascota.addCell("Fecha de nacimiento");
+		tablaMascota.addCell("Foto");
+		tablaMascota.setHeaderRows(1);
+		
+		PdfPTable tablaCitas = new PdfPTable(4);
 		tablaCitas.addCell("Fecha");
 		tablaCitas.addCell("Motivo");
 		tablaCitas.addCell("Informe");
 		tablaCitas.addCell("Realizada");
+		tablaCitas.setHeaderRows(1);
 		
+		document.open();
 		listadoCitas.forEach(cita -> {
+			tablaCliente.deleteBodyRows();
+			tablaCliente.addCell(cita.getIdMascota().getIdCliente().getNombre());
+			tablaCliente.addCell(cita.getIdMascota().getIdCliente().getApellidos());
+			tablaCliente.addCell(cita.getIdMascota().getIdCliente().getTelefono());
+			tablaCliente.addCell(cita.getIdMascota().getIdCliente().getUsername());
+			document.add(tablaCliente);
+			
+			tablaVeterinarios.deleteBodyRows();
+			tablaVeterinarios.addCell(cita.getIdVeterinario().getNombre());
+			tablaVeterinarios.addCell(cita.getIdVeterinario().getApellidos());
+			tablaVeterinarios.addCell(cita.getIdVeterinario().getTelefono());
+			tablaVeterinarios.addCell(cita.getIdVeterinario().getUsername());
+			document.add(tablaVeterinarios);
+			
+			tablaMascota.deleteBodyRows();
+			tablaMascota.addCell(cita.getIdMascota().getNombre());
+			tablaMascota.addCell(cita.getIdMascota().getTipo());
+			tablaMascota.addCell(cita.getIdMascota().getRaza());
+			tablaMascota.addCell(cita.getIdMascota().getFechaNacimiento().toString());
+			tablaMascota.addCell(cita.getIdMascota().getFoto());
+			document.add(tablaMascota);
+			
+			tablaCitas.deleteBodyRows();
 			tablaCitas.addCell(cita.getFecha().toString());
 			tablaCitas.addCell(cita.getMotivo());
 			tablaCitas.addCell(cita.getInforme());
@@ -41,9 +89,14 @@ public class CrearPdf extends AbstractPdfView{
 				
 				tablaCitas.addCell("No");
 			}
+			document.add(tablaCitas);
+			document.newPage();
 		});
 		
-		document.add(tablaCitas);
+		/*document.add(tablaCitas);
+		document.newPage();
+		document.add(new Paragraph("PAGINA"));*/
+		document.close();
 	}
 
 }
